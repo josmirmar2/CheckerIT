024994665a317e4519cc58bba36a08192f0c24ea
José Manuel Miret Martín <josmirmar2@alum.us.es>
2026-05-10 19:15:55 +0200

    fix(settings): uso unico de postgre y eliminacion sqLite

--- backend/checkerit/settings.py
+++ backend/checkerit/settings.py
@@ -61,29 +61,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'checkerit.wsgi.application'
 
-USE_POSTGRESQL = os.getenv('USE_POSTGRESQL', 'False') == 'True'
-
-if USE_POSTGRESQL:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv('DB_NAME', 'checkerit_db'),
-            'USER': os.getenv('DB_USER', 'postgres'),
-            'PASSWORD': os.getenv('DB_PASSWORD', 'postgres'),
-            'HOST': os.getenv('DB_HOST', 'localhost'),
-            'PORT': os.getenv('DB_PORT', '5432'),
-            'OPTIONS': {
-                'client_encoding': 'UTF8',
-            },
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'checkerit_db'),
+        'USER': os.getenv('DB_USER', 'postgres'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'postgres'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
+        'OPTIONS': {
+            'client_encoding': 'UTF8',
+        },
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
